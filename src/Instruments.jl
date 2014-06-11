17a6cb09669c2a3c755fa10d0216af57ef8001d9
Colm Ryan <colm@colmryan.org>
2014-06-10 21:38:08 -0400

    Add generic Insrument type and some methods. Export methods from Instruments module.

--- src/Instruments.jl
+++ src/Instruments.jl
@@ -1,5 +1,14 @@
 module Instruments
 
+import Base: write, read
+export Instrument, connect!, disconnect!, write, read, query
+
 # package code goes here
+include("visa/VISA.jl")
+
+#Setup global resource manager
+rm = viOpenDefaultRM()
+
+include("instrument.jl")
 
 end # module
---
+++ src/instrument.jl
@@ -0,0 +1,20 @@
+type Instrument 
+	handle::ViObject
+end
+
+function connect!(instr::Instrument, address::String) 
+	instr.handle = viOpen(rm, address)
+end	
+disconnect!(instr::Instrument) = viClose(instr.handle)
+
+
+#String reads and writes
+write(instr::Instrument, msg::ASCIIString) = viWrite(instr.handle, msg)
+read(instr::Instrument) = rstrip(bytestring(viRead(instr.handle)), ['\r', '\n'])
+
+function query(instr::Instrument, msg::ASCIIString; delay::Real=0)
+	write(instr, msg)
+	sleep(delay)
+	read(instr)
+end
+
