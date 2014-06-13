48ffde685974a6245c1a88d2522d244d7734e7eb
Colm Ryan <colm@colmryan.org>
2014-06-12 23:08:01 -0400

    Turn check_connected into a macro for cleaner one-line functions.
    Again hat-tip @blakejohnson for macro help

--- src/instrument.jl
+++ src/instrument.jl
@@ -26,16 +26,20 @@ end
 #String reads and writes
 check_connected(instr::Instrument) = @assert instr.connected "Instrument is not connected!"
 
-function write(instr::Instrument, msg::ASCIIString)
-	check_connected(instr)
-	viWrite(instr.handle, msg)
-end 
-
-
-function read(instr::Instrument)
-	check_connected(instr)
-	rstrip(bytestring(viRead(instr.handle; bufSize=instr.bufSize)), ['\r', '\n'])
+macro check_connected(ex)
+	funcproto = ex.args[1]
+	body = ex.args[2]
+	instrument_obj = funcproto.args[2]
+	checkbody = quote
+		check_connected($(instrument_obj))
+		$body
+	end
+	return Expr(:function, esc(funcproto), esc(checkbody))
 end
+
+@check_connected write(instr::Instrument, msg::ASCIIString) = viWrite(instr.handle, msg)
+
+@check_connected read(instr::Instrument) = rstrip(bytestring(viRead(instr.handle; bufSize=instr.bufSize)), ['\r', '\n'])
 
 function query(instr::Instrument, msg::ASCIIString; delay::Real=0)
 	write(instr, msg)
