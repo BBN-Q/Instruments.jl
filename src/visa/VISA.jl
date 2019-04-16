859cd5b2846389c2908cfef5026f849790fc44e9
gribeill <guilhem.ribeill@gmail.com>
2019-04-16 16:05:47 -0400

    Fix check_status macro

--- src/visa/VISA.jl
+++ src/visa/VISA.jl
@@ -6,9 +6,9 @@ include("constants.jl")
 #Helper macro to make VISA call and check the status for an error
 macro check_status(viCall)
 	return quote
-		status = $viCall
+		status = $(esc(viCall))
 		if status[2] < VI_SUCCESS
-			errMsg = codes[status]
+			errMsg = codes[status[2]]
 			error("VISA C call failed with status $(errMsg[1]): $(errMsg[2])")
 		end
 		status[1]
