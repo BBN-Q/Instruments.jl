f3f3495bb6046d8db5ddd6003a2f2a1f09ecff23
gribeill <guilhem.ribeill@gmail.com>
2019-04-16 16:26:54 -0400

    Remove version check.

--- src/init.jl
+++ src/init.jl
@@ -9,12 +9,10 @@ function __init__()
   copy!(visalib, pyvisa.PyVisaLibrary())
 
   pyvers = pyvisa.__version__
-  global version = try
+  global pyvisa_version = try
     vparse(pyvers)
   catch
     v"0.0.0"
   end
-
-  @assert version > v"0.3.0" "Instruments.jl only supports pyvisa-py v0.3.0 and above."
 
 end
