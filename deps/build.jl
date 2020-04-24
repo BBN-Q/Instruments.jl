0d66ce4718760ca0b1f049da01f2f26291bec308
iuliancioarca <iulian.cioarca@gmail.com>
2020-04-24 11:29:42 +0300

    avoid error during building if visa not available

--- deps/build.jl
+++ deps/build.jl
@@ -2,5 +2,11 @@ using BinDeps
 @BinDeps.setup
 
 visa = library_dependency("visa", aliases = ["visa64","VISA","/Library/Frameworks/VISA.framework/VISA"])
+visa_path_found = BinDeps._find_library(visa)
 
-@BinDeps.install Dict(:visa=>:libvisa)
+if ~isempty(visa_path_found)
+    @BinDeps.install Dict(:visa=>:libvisa)
+else
+    @warn "No VISA libraries found, please check VISA installation for visa64 library!"
+end
+
