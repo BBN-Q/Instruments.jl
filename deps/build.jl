2a759af351f3b690faf80c29cc05e8851b0fcea1
Carsten Sauerbrey <carsten.sauerbrey@gmail.com>
2020-05-08 23:45:52 +0200

    Adding R&S VISA (#16)
    * Support Rohde&Schwarz VISA under Linux
    
    * Added R&S Visa
    
    * Added description to build.jl regarding librsvisa and changed URL in docs

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,8 @@
 using BinDeps
 @BinDeps.setup
 
-visa = library_dependency("visa", aliases = ["visa64","VISA","/Library/Frameworks/VISA.framework/VISA"])
+visa = library_dependency("visa", aliases = ["visa64","VISA","/Library/Frameworks/VISA.framework/VISA", "librsvisa"])
+# librsvisa is the specific Rohde & Schwarz VISA library name
 visa_path_found = BinDeps._find_library(visa)
 
 if ~isempty(visa_path_found)
